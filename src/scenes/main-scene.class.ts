fe5c162215e6364452ae7b840dff8a64077f7488
Giorgi Kobalia <gqobalia00@gmail.com>
2025-03-28 14:13:10 +0400

    Refactor MainScene to streamline character initialization and loading of spritesheets

--- src/scenes/main-scene.class.ts
+++ src/scenes/main-scene.class.ts
@@ -16,138 +16,85 @@ import {
 } from "../constants";
 import { bg_manifest } from "../manifests";
 
+interface CharacterConfig {
+  name: string;
+  classRef:
+    | typeof Knight
+    | typeof Archer
+    | typeof Necromancer
+    | typeof Paladin
+    | typeof Ronin;
+}
+
 export class MainScene extends Phaser.Scene {
   private background!: Background;
-  private knight!: Knight;
-  private archer!: Archer;
-  private necromancer!: Necromancer;
-  private paladin!: Paladin;
-  private ronin!: Ronin;
+
+  private characters: {
+    [key: string]: Knight | Archer | Necromancer | Paladin | Ronin;
+  } = {};
 
   constructor() {
     super("MainScene");
   }
 
   preload() {
-    // preload Background manifest
-    bg_manifest.forEach((element: { key: string; path: string }) => {
-      this.load.image(element.key, element.path);
-    });
-    // preload Knight manifest
-    knight_constants.forEach(
-      (element: {
+    const loadSpritesheets = (
+      constants: {
         spriteKey: string;
         spritePath: string;
         spriteConfiguration: Phaser.Types.Textures.SpriteSheetConfig;
-      }) => {
+      }[]
+    ) => {
+      constants.forEach((element) => {
         this.load.spritesheet(
           element.spriteKey,
           element.spritePath,
           element.spriteConfiguration
         );
-      }
-    );
-    // preload Archer manifest
-    archer_constants.forEach(
-      (element: {
-        spriteKey: string;
-        spritePath: string;
-        spriteConfiguration: Phaser.Types.Textures.SpriteSheetConfig;
-      }) => {
-        this.load.spritesheet(
-          element.spriteKey,
-          element.spritePath,
-          element.spriteConfiguration
-        );
-      }
-    );
-    // preload Necromancer manifest
-    necromancer_constants.forEach(
-      (element: {
-        spriteKey: string;
-        spritePath: string;
-        spriteConfiguration: Phaser.Types.Textures.SpriteSheetConfig;
-      }) => {
-        this.load.spritesheet(
-          element.spriteKey,
-          element.spritePath,
-          element.spriteConfiguration
-        );
-      }
-    );
-    // preload Paladin manifest
-    paladin_constants.forEach(
-      (element: {
-        spriteKey: string;
-        spritePath: string;
-        spriteConfiguration: Phaser.Types.Textures.SpriteSheetConfig;
-      }) => {
-        this.load.spritesheet(
-          element.spriteKey,
-          element.spritePath,
-          element.spriteConfiguration
-        );
-      }
-    );
+      });
+    };
 
-    // preload Ronin manifest
-    ronin_constants.forEach(
-      (element: {
-        spriteKey: string;
-        spritePath: string;
-        spriteConfiguration: Phaser.Types.Textures.SpriteSheetConfig;
-      }) => {
-        this.load.spritesheet(
-          element.spriteKey,
-          element.spritePath,
-          element.spriteConfiguration
-        );
-      }
-    );
-
-    // preload bullets manifest
     bullets_constants.forEach(
       (element: { spriteKey: string; spritePath: string }) => {
         this.load.image(element.spriteKey, element.spritePath);
       }
     );
+    bg_manifest.forEach((element: { key: string; path: string }) => {
+      this.load.image(element.key, element.path);
+    });
+
+    // Preload all character spritesheets
+    loadSpritesheets(knight_constants);
+    loadSpritesheets(archer_constants);
+    loadSpritesheets(necromancer_constants);
+    loadSpritesheets(paladin_constants);
+    loadSpritesheets(ronin_constants);
   }
 
   create() {
     this.background = new Background(this);
 
-    this.knight = new Knight(this, this.background);
-
-    this.archer = new Archer(this);
-
-    this.necromancer = new Necromancer(this);
-
-    this.paladin = new Paladin(this);
-
-    this.ronin = new Ronin(this);
+    const characters: CharacterConfig[] = [
+      { name: "knight", classRef: Knight },
+      { name: "archer", classRef: Archer },
+      { name: "necromancer", classRef: Necromancer },
+      { name: "paladin", classRef: Paladin },
+      { name: "ronin", classRef: Ronin },
+    ];
 
     this.background.init();
 
-    this.knight.init();
-
-    this.archer.init();
-
-    this.necromancer.init();
-
-    this.paladin.init();
-
-    this.ronin.init();
+    characters.forEach((character) => {
+      const characterInstance = new character.classRef(this, this.background);
+      characterInstance.init();
+      this.characters[character.name] = characterInstance;
+    });
   }
 
   update() {
-    this.knight.update();
-
-    this.archer.update();
-
-    this.necromancer.update();
-
-    this.paladin.update();
-
-    this.ronin.update();
+    // Iterate over all character instances and call update
+    Object.values(this.characters).forEach((character) => {
+      character.update();
+    });
   }
 }
