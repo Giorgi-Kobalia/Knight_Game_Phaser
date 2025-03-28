36e0ac4bd67d2b451ff67327e1f9e1f07684cfc6
Giorgi Kobalia <gqobalia00@gmail.com>
2025-03-28 12:48:17 +0400

    Add Necromancer skull sprite and bullets constants; remove old skull sprite

--- src/constants/bullets.constants.ts
+++ src/constants/bullets.constants.ts
@@ -1,7 +1,7 @@
 export const bullets_constants = [
   {
     spriteKey: "Skull",
-    spritePath: "./img/Necromancer/skull2.png",
+    spritePath: "./img/Necromancer/skull.png",
   },
   {
     spriteKey: "Arrow",
